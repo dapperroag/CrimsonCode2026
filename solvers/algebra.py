b1a9767c65d90038c96775ce970f456610f1f9ca
dapperroag <roagmine@gmail.com>
2026-02-21 18:55:49 -0800

    Added algebra logic & get variables

--- solvers/algebra.py
+++ solvers/algebra.py
@@ -1,7 +1,25 @@
+import sympy as sp
 from .base import BaseSolver
 
 class AlgebraSolver(BaseSolver):
     def solve(self, user_input: str):
-        user_input = self.cleanse_input(user_input)
-        expr = self.to_sp_object(user_input)
-        print(expr)
+        clean_input = self.cleanse_input(user_input)
+
+        #We are dealing with an equation
+        if "=" in clean_input:
+            parts = clean_input.split("=")
+            left_side = sp.sympify(parts[0])
+            right_side = sp.sympify(parts[1])
+            equation = sp.Eq(left_side, right_side)
+        #Dealing with an expression
+        else:
+            equation = sp.sympify(clean_input)
+
+        vars = self.get_variables(equation)
+
+        if not vars:
+            print(equation)
+            return equation
+        else:
+            print(sp.solve(equation, vars))
+            return sp.solve(equation, vars)
